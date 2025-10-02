c973825753c842e1b5292372e3232e70585abcee
yudidev <yudigustavo813@gmail.com>
2025-10-02 17:02:43 -0300

    finalizei teoricamente o js

---
+++ script.js
@@ -0,0 +1,13 @@
+const amountInput = document.getElementById("amount");
+
+const currencySelect = document.getElementById("opcao")
+
+const button = document.getElementById("convertButton")
+
+const span = document.getElementById("conversao_resultado")
+
+button.addEventListener ("click"), () => {
+  const amount = parseFloat(amountInput.value);
+  const currency = currencySelect.value;
+  console.log(amount, currency);
+}
