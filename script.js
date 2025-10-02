eb9888053de0c104b87b1790b86793a66ed370b9
yudidev <yudigustavo813@gmail.com>
2025-10-02 17:07:24 -0300

    agora sim debugado e funcionando a função

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const button = document.getElementById("convertButton")
 
 const span = document.getElementById("conversao_resultado")
 
-button.addEventListener ("click"), () => {
+button.addEventListener ("click", () => {
   const amount = parseFloat(amountInput.value);
   const currency = currencySelect.value;
   console.log(amount, currency);
-}
+});
